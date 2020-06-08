ba45f8ed0aacdaa34319422d5e05cbdafcecffef
valeriö <valeriodambrosio.14@gmail.com>
2020-06-08 13:51:03 +0200

    Completato esercizio

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
-$().show(function() {
-  alert();
-});
-$().hide(function() {
-  alert();
+//al click su icona hamburger appendo classe visible con proprietà display block
+$(".fas.fa-bars").click(function() {
+
+  $(".hamburger-menu").addClass("visible");
+
 });
 
+//al click su icona close rimuovo classe visible
+$(".fas.fa-times").click(function() {
 
-$().addClass();
-$().removeClass();
+  $(".hamburger-menu").removeClass("visible");
+
+});
